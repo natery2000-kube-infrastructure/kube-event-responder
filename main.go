d1966a73e62bb51c08221cb26e9ee0a1e260c1a0
Nathan <you@example.com>
2022-07-10 13:53:24 -0500

    attemping two handlers

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	api_v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -21,13 +23,33 @@ type Config struct {
 }
 
 type Event struct {
-	key          string
-	eventType    string
+	resourceName string
+	action       string
 	resourceType string
 }
 
 func main() {
 	conf := Config{Namespace: "default"}
+	eventChan := make(chan Event)
+	var handlers []Handler
+
+	kubectlCommandHandler := KubectlCommandHandler{
+		command: "kubectl get pods",
+		HandlerBase: HandlerBase{
+			action:       "Update",
+			resourceName: "Deployment/streaming-ui-deployment",
+		},
+	}
+	handlers = append(handlers, kubectlCommandHandler)
+
+	printlnHandler := PrintlnHandler{
+		printString: "hello world",
+		HandlerBase: HandlerBase{
+			action:       "Update",
+			resourceName: "Deployment/streaming-ui-deployment",
+		},
+	}
+	handlers = append(handlers, printlnHandler)
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -42,9 +64,6 @@ func main() {
 	//if conf.Resource == "ConfigMap" {
 	informer := cache.NewSharedIndexInformer(
 		&cache.ListWatch{
-			ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
-				return kubeClient.CoreV1().ConfigMaps(conf.Namespace).List(context.TODO(), options)
-			},
 			WatchFunc: func(options meta_v1.ListOptions) (watch.Interface, error) {
 				return kubeClient.CoreV1().ConfigMaps(conf.Namespace).Watch(context.TODO(), options)
 			},
@@ -57,31 +76,83 @@ func main() {
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			var newEvent Event
-			newEvent.key, err = cache.MetaNamespaceKeyFunc(obj)
-			newEvent.eventType = "create"
+			newEvent.resourceName, err = cache.MetaNamespaceKeyFunc(obj)
+			newEvent.action = "create"
 			newEvent.resourceType = "configMap"
-			fmt.Println(newEvent)
+			eventChan <- newEvent
 		},
 		UpdateFunc: func(old, new interface{}) {
 			var newEvent Event
-			newEvent.key, err = cache.MetaNamespaceKeyFunc(old)
-			newEvent.eventType = "update"
+			newEvent.resourceName, err = cache.MetaNamespaceKeyFunc(old)
+			newEvent.action = "update"
 			newEvent.resourceType = "configMap"
-			fmt.Println(newEvent)
+			eventChan <- newEvent
 		},
 		DeleteFunc: func(obj interface{}) {
 			var newEvent Event
-			newEvent.key, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			newEvent.eventType = "delete"
+			newEvent.resourceName, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			newEvent.action = "delete"
 			newEvent.resourceType = "configMap"
-			fmt.Println(newEvent)
+			eventChan <- newEvent
 		},
 	})
+
+	go worker(eventChan, handlers)
 
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 	informer.Run(stopCh)
+}
 
-	//wait.Until(nil, time.Second, stopCh)
-	//}
+func worker(eventChan <-chan Event, handlers []Handler) {
+	for event := range eventChan {
+		for _, handler := range handlers {
+			fmt.Println("event receieved", event)
+			handler.Handle(event)
+		}
+	}
+}
+
+type Handler interface {
+	Handle(e Event)
+}
+
+type HandlerBase struct {
+	action       string
+	resourceName string
+}
+
+type KubectlCommandHandler struct {
+	HandlerBase
+	command string
+}
+
+func (handler KubectlCommandHandler) Handle(e Event) {
+	if !(handler.action == e.action && handler.resourceName == e.resourceName) {
+		return
+	}
+
+	var cmd *exec.Cmd
+	if strings.Contains(handler.command, " ") {
+		parts := strings.Split(handler.command, " ")
+		cmd = exec.Command(parts[0], parts[1:]...)
+	} else {
+		cmd = exec.Command(handler.command)
+	}
+	cmd.Dir = "."
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
+type PrintlnHandler struct {
+	HandlerBase
+	printString string
+}
+
+func (handler PrintlnHandler) Handle(e Event) {
+	if !(handler.action == e.action && handler.resourceName == e.resourceName) {
+		return
+	}
+	fmt.Println(handler.printString, e)
 }
