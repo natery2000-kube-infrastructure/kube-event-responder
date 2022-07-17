95b391bdf089c2df91ff63067e86e86ae6bcf88d
Nathan <you@example.com>
2022-07-17 09:51:35 -0500

    Adding metadata to logging

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"os/exec"
 	"strings"
 
+	apps_v1 "k8s.io/api/apps/v1"
+	batch_v1 "k8s.io/api/batch/v1"
 	api_v1 "k8s.io/api/core/v1"
+	ext_v1beta1 "k8s.io/api/extensions/v1beta1"
+	rbac_v1beta1 "k8s.io/api/rbac/v1beta1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -81,26 +85,29 @@ func main() {
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			var newEvent Event
+			objectMeta := GetObjectMetaData(obj)
 			newEvent.resourceName, err = cache.MetaNamespaceKeyFunc(obj)
 			newEvent.action = "create"
 			newEvent.resourceType = "configMap"
-			newEvent.raw = obj
+			newEvent.raw = objectMeta
 			eventChan <- newEvent
 		},
 		UpdateFunc: func(old, new interface{}) {
 			var newEvent Event
+			objectMeta := GetObjectMetaData(old)
 			newEvent.resourceName, err = cache.MetaNamespaceKeyFunc(old)
 			newEvent.action = "update"
 			newEvent.resourceType = "configMap"
-			newEvent.raw = new
+			newEvent.raw = objectMeta
 			eventChan <- newEvent
 		},
 		DeleteFunc: func(obj interface{}) {
 			var newEvent Event
+			objectMeta := GetObjectMetaData(obj)
 			newEvent.resourceName, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
 			newEvent.action = "delete"
 			newEvent.resourceType = "configMap"
-			newEvent.raw = obj
+			newEvent.raw = objectMeta
 			eventChan <- newEvent
 		},
 	})
@@ -166,4 +173,42 @@ func (handler PrintlnHandler) Handle(e Event) {
 		return
 	}
 	fmt.Println(handler.printString, e)
+}
+
+// GetObjectMetaData returns metadata of a given k8s object
+func GetObjectMetaData(obj interface{}) (objectMeta meta_v1.ObjectMeta) {
+
+	switch object := obj.(type) {
+	case *apps_v1.Deployment:
+		objectMeta = object.ObjectMeta
+	case *api_v1.ReplicationController:
+		objectMeta = object.ObjectMeta
+	case *apps_v1.ReplicaSet:
+		objectMeta = object.ObjectMeta
+	case *apps_v1.DaemonSet:
+		objectMeta = object.ObjectMeta
+	case *api_v1.Service:
+		objectMeta = object.ObjectMeta
+	case *api_v1.Pod:
+		objectMeta = object.ObjectMeta
+	case *batch_v1.Job:
+		objectMeta = object.ObjectMeta
+	case *api_v1.PersistentVolume:
+		objectMeta = object.ObjectMeta
+	case *api_v1.Namespace:
+		objectMeta = object.ObjectMeta
+	case *api_v1.Secret:
+		objectMeta = object.ObjectMeta
+	case *ext_v1beta1.Ingress:
+		objectMeta = object.ObjectMeta
+	case *api_v1.Node:
+		objectMeta = object.ObjectMeta
+	case *rbac_v1beta1.ClusterRole:
+		objectMeta = object.ObjectMeta
+	case *api_v1.ServiceAccount:
+		objectMeta = object.ObjectMeta
+	case *api_v1.Event:
+		objectMeta = object.ObjectMeta
+	}
+	return objectMeta
 }
